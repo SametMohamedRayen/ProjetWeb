215cb2f980d00042a49247c5d88a29f6a37ccf87
SametMohamedRayen <rayensamet77@gmail.com>
2021-05-29 01:56:54 +0100

    Formulaire d'ajout

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Evenement;
+use App\Entity\Indoor;
 use App\Form\EndroitType;
 use App\Entity\Endroit;
+use App\Form\EventType;
+use App\Form\IndoorFType;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -40,7 +46,12 @@ class ActivityController extends AbstractController
      */
     public function formind()
     {
-        //FORM FOR INDOOR
+        $ind = new Indoor();
+        $form = $this->createForm(IndoorFType::class,$ind);
+        return($this->render('activity/indForm.html.twig',[
+            'ind'=>$ind,
+            'form'=>$form->createView()
+        ]));
     }
 
     /**
@@ -48,16 +59,28 @@ class ActivityController extends AbstractController
      */
     public function formevn()
     {
-        //FORM EVENT
+        $event = new Evenement();
+        $form = $this->createForm(EventType::class,$event);
+        return($this->render('activity/eventForm.html.twig',[
+            'event'=>$event,
+            'form'=>$form->createView()
+        ]));
     }
 
     /**
      * @Route("/inoout/evnend/end",name="end")
      */
-    public function formend()
+    public function formend(EntityManagerInterface $manager)
     {
         $endroit = new Endroit();
         $form = $this->createForm(EndroitType::class,$endroit);
+        if($form->isSubmitted())
+        {
+            dd($endroit);
+            //$manager->persist($endroit);
+            //$manager->flush();
+            //$this->addFlash('succes','L\'endroit ajouté avec succées');
+        }
         return($this->render('activity/endroitForm.html.twig',[
             'endroit' => $endroit,
             'form' => $form->createView()

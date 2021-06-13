f4026cb6cdb5191cace91fddd27100949fdae22d
TasnimDakhli <tasnimdakhli@insat.u-carthage.tn>
2021-06-13 15:43:37 +0100

    some changes

--- migrations/Version20210612183924.php
+++ migrations/Version20210612183924.php
@@ -20,12 +20,12 @@ final class Version20210612183924 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE event ADD price_max DOUBLE PRECISION NOT NULL, CHANGE price price_min DOUBLE PRECISION NOT NULL');
+        //$this->addSql('ALTER TABLE event ADD price_max DOUBLE PRECISION NOT NULL, CHANGE price price_min DOUBLE PRECISION NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE event ADD price DOUBLE PRECISION NOT NULL, DROP price_min, DROP price_max');
+        //$this->addSql('ALTER TABLE event ADD price DOUBLE PRECISION NOT NULL, DROP price_min, DROP price_max');
     }
 }
